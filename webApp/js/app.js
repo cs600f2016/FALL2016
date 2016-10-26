bd2caee9a9ec48f7368602c414441630e69a2308
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2016-10-26 15:20:15 -0400

    added token generator and FB team structure

--- webApp/js/app.js
+++ webApp/js/app.js
@@ -10,7 +10,6 @@
   firebase.initializeApp(config);
 
 
-
 const txtEmail = document.getElementById('email');
 const txtPassword = document.getElementById('password');
 const btnLogin = document.getElementById('btnLogin');
@@ -46,7 +45,7 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 		console.log(firebaseUser);
 		loginDiv.classList.add('hide');
 		logoutDiv.classList.remove('hide');
-
+		window.location='/user/loggedIn.html';
 
 	}
 	else {
@@ -60,6 +59,38 @@ btnLogout.addEventListener('click', e=> {
 	firebase.auth().signOut();
 
 })
+
+
+
+	// set the length of the string
+	var stringLength = 5;
+
+	// list containing characters for the random string
+	var stringArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','?'];
+
+
+		var rndString = "";
+	
+		// build a string with random characters
+		for (var i = 1; i < stringLength; i++) { 
+			var rndNum = Math.ceil(Math.random() * stringArray.length) - 1;
+			rndString = rndString + stringArray[rndNum];
+		};
+		console.log(rndString);
+	
+
+
+var theTeam = firebase.database().ref("/Teams");
+theTeam.push({
+			"Name": rndString,
+			"Member 1" : "",
+			"Member 2" : "",
+			"Member 3" : "",
+			"Member 4" : ""
+
+});
+
+
 
 }());
 
