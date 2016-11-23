a1e3e64d84e42b09d1decddbf1d111277d191f4c
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2016-11-22 20:11:04 -0500

    correctly adds team members by email

--- webApp/js/startTeam.js
+++ webApp/js/startTeam.js
@@ -89,9 +89,9 @@
       var user = firebase.auth().currentUser;
                       var updateTeam = {[rndString] : {
                         "Member1" : user.email,
-                        "Member2" : "RogerTheRabbit",
-                        "Member3" : "EllenTheElephant",
-                        "Member4" : "JessTheJaguar",
+                        "Member2" : "",
+                        "Member3" : "",
+                        "Member4" : "",
                         "name" : rndString
                       }};
                       theTeam.update(updateTeam);
