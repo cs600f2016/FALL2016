c5e364791e80d05ebfe26127988d1d5fcff2c2bd
Vanessa Rene <vjrene@gmail.com>
2016-12-04 19:35:01 -0500

    checking items off material list

--- webApp/js/gameplay.js
+++ webApp/js/gameplay.js
@@ -16,20 +16,24 @@
     const mat3 = document.getElementById("mat3");
     const mat4 = document.getElementById("mat4");
     
-  var ref = firebase.database().ref('/Materials');
+  var ref = firebase.database().ref('Materials/');
 
   var materials = { 
-    "Material1" : "8 Sticks",
+      
+    "Material1" : "8 sticks",
+      "Material1Found":0,
     "Material2" : "3 Soda Cans",
+      "Material2Found":0,
     "Material3" : "1 Fabian",
-    "Material4" : "2 pieces of litter"
+      "Material3Found":0,
+    "Material4" : "2 pieces of litter",
+      "Material4Found":0
   };
     
     ref.update(materials);
     pullMaterials();
     
     function parseData(Material1, Material2, Material3, Material4){
-        console.log("here");
         mat1.innerHTML=Material1;
         mat2.innerHTML=Material2;
         mat3.innerHTML=Material3;
@@ -40,14 +44,68 @@
         ref.orderByChild("name").on("value", function(snapshot) {
 
           var matSnapshot = snapshot.val();
-          console.log(matSnapshot);
-            
+
         parseData(matSnapshot.Material1, matSnapshot.Material2,
             matSnapshot.Material3, matSnapshot.Material4);
         }, function (errorObject) {
           console.log("The read failed: " + errorObject.code);
         });
   }
+    
+    
+    mat1.addEventListener('click', e=> {
+        if(materials.Material1Found == 0){
+            materials.Material1Found = 1;
+            mat1.style.textDecoration = "line-through";
+            ref.update(materials);
+        }
+        else {
+            materials.Material1Found = 0;
+            mat1.style.textDecoration = "none";
+            ref.update(materials);
+        } 
+    });
+    
+mat2.addEventListener('click', e=> {
+        if(materials.Material2Found == 0){
+            materials.Material2Found = 1;
+            mat2.style.textDecoration = "line-through";
+            ref.update(materials);
+        }
+        else {
+            materials.Material2Found = 0;
+            mat2.style.textDecoration = "none";
+            ref.update(materials);
+        } 
+    });
 
+mat3.addEventListener('click', e=> {
+        if(materials.Material3Found == 0){
+            materials.Material3Found = 1;
+            mat3.style.textDecoration = "line-through";
+            ref.update(materials);
+        }
+        else {
+            materials.Material2Found = 0;
+            mat3.style.textDecoration = "none";
+            ref.update(materials);
+        } 
+    });
+    
+mat4.addEventListener('click', e=> {
+        if(materials.Material4Found == 0){
+            materials.Material4Found = 1;
+            mat4.style.textDecoration = "line-through";
+            ref.update(materials);
+        }
+        else {
+            materials.Material2Found = 0;
+            mat4.style.textDecoration = "none";
+            ref.update(materials);
+        } 
+    });
+    
+
+    
 
 }());
